36c40dd33b9224515799528a78a40e6531a07cab
Oksana Oberemok <Pylypenkok@gmail.com>
2023-04-07 14:51:49 +0700

    use props in Card

--- components/Card.js
+++ components/Card.js
@@ -1,16 +1,22 @@
 import Image from "next/image";
 import styles from "../styles/Card.module.css";
+import Link from "next/link";
 
 const Card = (props) => {
   return (
     <div className={styles.card}>
-      <Image height="250" width="250" src="/images/pullup.jpg" alt="pull up" />
-      <h4 className={styles.title}>Upper body workout</h4>
-      <p className={styles.description}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
-        laboriosam!
-      </p>
-      <button className={styles.button}>learn more</button>
+      <Image
+        height="270"
+        width="220"
+        src={props.image}
+        alt="pull up"
+        className={styles.image}
+      />
+      <h4 className={styles.title}>{props.subtitle}</h4>
+      <p className={styles.description}>{props.description}</p>
+      <Link href={`/workouts/${props.id}`} className={styles.button}>
+        learn more
+      </Link>
     </div>
   );
 };

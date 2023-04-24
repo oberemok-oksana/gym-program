de356fb8bc7b043f493ca3d10b36576b3bf14d94
Oksana Oberemok <Pylypenkok@gmail.com>
2023-04-24 17:08:45 +0700

    implement localStorage for sets,reps,weights

--- pages/programs/[id].js
+++ pages/programs/[id].js
@@ -3,9 +3,55 @@ import styles from "../../styles/Program.module.css";
 
 import { getAllProgramsIds, getProgramData } from "@/lib/programs";
 import Footer from "@/components/Footer";
+import { useEffect, useState } from "react";
+
+function isClient() {
+  return typeof window !== "undefined";
+}
 
 const Program = ({ program }) => {
-  console.log(program);
+  const initialWorkout = [
+    { sets: 0, reps: 0, weights: 0 },
+    { sets: 0, reps: 0, weights: 0 },
+    { sets: 0, reps: 0, weights: 0 },
+    { sets: 0, reps: 0, weights: 0 },
+    { sets: 0, reps: 0, weights: 0 },
+  ];
+  const programId = program.id;
+  const [workout, setWorkout] = useState(
+    isClient()
+      ? JSON.parse(localStorage.getItem(programId)) ?? initialWorkout
+      : initialWorkout
+  );
+
+  useEffect(() => {
+    localStorage.setItem(programId, JSON.stringify(workout));
+  }, [workout, programId]);
+
+  const changeSets = (i, value) => {
+    setWorkout((oldWorkout) => {
+      return oldWorkout.map((arr, index) => {
+        return i === index ? { ...arr, sets: value } : arr;
+      });
+    });
+  };
+
+  const changeReps = (i, value) => {
+    setWorkout((oldWorkout) => {
+      return oldWorkout.map((arr, index) => {
+        return i === index ? { ...arr, reps: value } : arr;
+      });
+    });
+  };
+
+  const changeWeights = (i, value) => {
+    setWorkout((oldWorkout) => {
+      return oldWorkout.map((arr, index) => {
+        return i === index ? { ...arr, weights: value } : arr;
+      });
+    });
+  };
+
   return (
     <>
       <div className={styles.bg}>
@@ -51,18 +97,35 @@ const Program = ({ program }) => {
                 </tr>
               </thead>
               <tbody>
-                {program.exercises.map((exercise) => (
+                {program.exercises.map((exercise, i) => (
                   <tr key={exercise.exercise}>
                     <td className="table-cell">{exercise.exercise}</td>
                     <td className="table-cell">
-                      <input className={styles.input} type="number" />
+                      <input
+                        className={styles.input}
+                        type="number"
+                        name="sets"
+                        value={workout[i].sets}
+                        onChange={(e) => changeSets(i, e.target.value)}
+                      />
                     </td>
                     <td className="table-cell">
-                      <input className={styles.input} type="number" />
+                      <input
+                        className={styles.input}
+                        type="number"
+                        name="reps"
+                        value={workout[i].reps}
+                        onChange={(e) => changeReps(i, e.target.value)}
+                      />
                     </td>
                     <td>
-                      {" "}
-                      <input className={styles.input} type="number" />
+                      <input
+                        className={styles.input}
+                        type="number"
+                        name="weights"
+                        value={workout[i].weights}
+                        onChange={(e) => changeWeights(i, e.target.value)}
+                      />
                     </td>
                   </tr>
                 ))}

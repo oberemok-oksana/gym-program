d2b6db179e24f0d11b7a7e79195d7053f1f83587
Oksana Oberemok <Pylypenkok@gmail.com>
2023-04-24 14:16:33 +0700

    add Header, Footer

--- pages/programs/index.js
+++ pages/programs/index.js
@@ -1,5 +1,10 @@
 import { getSortedProgramsData } from "@/lib/programs";
 import Program from "./[id]";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import styles from "../../styles/Program.module.css";
+import Exercise from "@/components/Exercise";
+import Layout from "@/components/Layout";
 
 export const getStaticProps = () => {
   const allProgramsData = getSortedProgramsData();
@@ -13,14 +18,30 @@ export const getStaticProps = () => {
 
 const Programs = ({ allProgramsData }) => {
   return (
-    <ul>
-      {allProgramsData.map((program) => (
-        <li key={program.id}>
-          {" "}
-          <Program programData={program} />
-        </li>
-      ))}
-    </ul>
+    <div className={styles.bg}>
+      <div className="container">
+        <Header />
+        <div className={styles.wrapper}>
+          <h1 className={styles.title}>All Programs</h1>
+        </div>
+        <ul className={styles.cards}>
+          {allProgramsData.map((programData) => (
+            <li key={programData.id}>
+              <Exercise program={programData} />
+              {/* <Program program={programData} /> */}
+            </li>
+          ))}
+        </ul>
+        {/* <ul>
+        {allProgramsData.map((program) => (
+          <li key={program.id}>
+            <Program programData={program} />
+          </li>
+        ))}
+      </ul> */}
+        <Footer />
+      </div>
+    </div>
   );
 };
 

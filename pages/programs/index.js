9587463729a0fd72c05e59408942cc74b8de7400
Oksana Oberemok <Pylypenkok@gmail.com>
2023-04-24 17:21:24 +0700

    delete comments

--- pages/programs/index.js
+++ pages/programs/index.js
@@ -1,10 +1,8 @@
 import { getSortedProgramsData } from "@/lib/programs";
-import Program from "./[id]";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import styles from "../../styles/Program.module.css";
 import Exercise from "@/components/Exercise";
-import Layout from "@/components/Layout";
 
 export const getStaticProps = () => {
   const allProgramsData = getSortedProgramsData();
@@ -28,17 +26,9 @@ const Programs = ({ allProgramsData }) => {
           {allProgramsData.map((programData) => (
             <li key={programData.id}>
               <Exercise program={programData} />
-              {/* <Program program={programData} /> */}
             </li>
           ))}
         </ul>
-        {/* <ul>
-        {allProgramsData.map((program) => (
-          <li key={program.id}>
-            <Program programData={program} />
-          </li>
-        ))}
-      </ul> */}
         <Footer />
       </div>
     </div>
